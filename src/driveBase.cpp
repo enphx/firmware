8ad4babcc95b65b6cb2e388bacd4523f42e03c9b
Yuvraj Cheema <yuvrajcheema@dhcp-128-189-208-189.ubcsecure.wireless.ubc.ca>
2025-07-16 16:34:26 -0700

    created framework

---
+++ src/driveBase.cpp
@@ -0,0 +1,34 @@
+#include "driveBase.h"
+
+// TODO: Write DriveBase Control
+DriveBase::DriveBase(void) {}
+
+void DriveBase::begin() {
+  timeLastUpdated = micros();
+}
+
+void DriveBase::setLineFollowingPID(float m_Kp, float m_Ki, float m_Kd) {
+  Kp = m_Kp;
+  Ki = m_Ki;
+  Kd = m_Kd;
+}
+
+void DriveBase::update(void) {
+  deltaT = micros() - timeLastUpdated;
+  timeLastUpdated += deltaT;
+}
+void setBaseSpeed(float speed) {}
+
+const tapeState DriveBase::getTapeState(void) {
+  return Distance;
+}
+
+const tapeState  DriveBase::getSide(void) {
+  return Side;
+}
+
+float DriveBase::calculateError(void) {
+  return 0.0f;
+}
+
+void DriveBase::findTape(void) {}
