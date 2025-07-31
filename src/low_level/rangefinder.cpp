92472252a4e05dd4c31f67572cee10cb257705ff
Yuvraj Cheema <yuvrajcheema@dhcp-128-189-211-175.ubcsecure.wireless.ubc.ca>
2025-07-31 13:28:40 -0700

    rangefinder

--- src/low_level/rangefinder.cpp
+++ src/low_level/rangefinder.cpp
@@ -1,10 +1,9 @@
 #include "include/low_level/rangefinder.h"
 #include "include/low_level/io.h"
 
+static const char *TAG = "RANGEFINDER";
 
-static const char* TAG = "RANGEFINDER";
-
-RangeFinder::RangeFinder(uint8_t i2c_addr) : addr(i2c_addr)  {}
+RangeFinder::RangeFinder(uint8_t i2c_addr) : addr(i2c_addr) {}
 
 void RangeFinder::init() {
   if (!vl53.begin(I2C_RANGEFINDER_ADDR)) {
@@ -15,7 +14,11 @@ void RangeFinder::init() {
   } else {
     ESP_LOGI(TAG, "Rangefinder found successfully at address %u", addr);
   }
+  vl53.VL53L1X_SetDistanceMode(1);
 
+  vl53.VL53L1X_SetTimingBudgetInMs(20);
+
+  vl53.VL53L1X_SetInterMeasurementInMs(30);
   if (!vl53.startRanging()) {
     while (1) {
       ESP_LOGE(TAG, "Couldn't start ranging...");
@@ -23,11 +26,7 @@ void RangeFinder::init() {
     }
   }
 
-  vl53.setTimingBudget(50);
-
   ESP_LOGI(TAG, "Started ranging successfully!!");
 }
 
-int16_t RangeFinder::getDistance() {
-  return vl53.distance();
-}
+int16_t RangeFinder::getDistance() { return vl53.distance(); }
