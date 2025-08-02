1226899a4963576d2827d5da58d530c8e1e4ae0e
Yuvraj Cheema <yuvrajcheema@dhcp-128-189-215-155.ubcsecure.wireless.ubc.ca>
2025-08-01 21:21:35 -0700

    thweshold twipped uwu

--- include/scanner.h
+++ include/scanner.h
@@ -1,32 +1,31 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
-#include <Arduino.h>
-#include <cstring>
 #include "convfilter.h"
 #include "include/robotposition.h"
+#include <Arduino.h>
+#include <cstring>
 
 struct ScannerPoint {
   int16_t distance;
-  int32_t convolved;  
+  int32_t convolved;
   RobotPosition position;
 };
 
-
-
 #define KERN_SIZE 2
 static int32_t kern1[KERN_SIZE] = {
-  -1,
-  1,
+    -1,
+    1,
 };
-
 
 #define BUF_SIZE 1000
 
 class Scanner {
 public:
   inline ScannerPoint getVal(uint32_t index) {
-    return index < BUF_SIZE ? buffer[index] : ScannerPoint {-1,-1,{{-1,-1,-1},{-1,-1,-1}}};
+    return index < BUF_SIZE
+               ? buffer[index]
+               : ScannerPoint{-1, -1, {{-1, -1, -1}, {-1, -1, -1}}};
   }
 
   inline void reset() {
@@ -39,14 +38,25 @@ public:
 
   void print_log();
 
+  inline void setThreshold(uint16_t threshold, bool tiggerAbove) {
+    thresholdValue = threshold;
+    triggerAboveThreshold = tiggerAbove;
+    thresholdTripped = false;
+  }
+
+  inline bool thresholdTripper(void) {return thresholdTripped;}
+
+
 private:
   ScannerPoint buffer[BUF_SIZE];
   uint32_t buf_index = 0;
   bool first_value = true;
+  bool thresholdTripped = false;
+  bool triggerAboveThreshold = false;
+  uint16_t thresholdValue = 4000;
 
-  ConvFilter<int32_t, KERN_SIZE> conv_filter = ConvFilter<int32_t, KERN_SIZE>(kern1);
-
- 
+  ConvFilter<int32_t, KERN_SIZE> conv_filter =
+      ConvFilter<int32_t, KERN_SIZE>(kern1);
 };
 
 #endif
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -3,7 +3,7 @@
 #include "include/constants.h"
 #include "include/serial/serial_comms.h"
 
-static const char* TAG = "SCANNER";
+static const char *TAG = "SCANNER";
 
 ScannerPoint Scanner::push(int16_t distance, RobotPosition robot_position) {
   if (first_value) {
@@ -13,20 +13,25 @@ ScannerPoint Scanner::push(int16_t distance, RobotPosition robot_position) {
     first_value = false;
   }
 
+  if ((distance < (int)thresholdValue && !triggerAboveThreshold) ||
+      (distance > (int)thresholdValue && triggerAboveThreshold)) {
+    thresholdTripped = true;
+  }
+
   ScannerPoint output = {
-    distance,
-    conv_filter.eval(distance),
-    robot_position,
+      distance,
+      conv_filter.eval(distance),
+      robot_position,
   };
 
   buffer[buf_index] = output;
   // buf_index = (buf_index + 1) >= BUF_SIZE ? BUF_SIZE - 1 : buf_index + 1;
- 
+
   return output;
 }
 
 void Scanner::print_log() {
-  #ifdef SERIAL_OUTPUT
+#ifdef SERIAL_OUTPUT
 
   uint8_t message_to_send[256];
 
@@ -36,7 +41,7 @@ void Scanner::print_log() {
     copy_1(&message_to_send[i], MSG_START, &i);
     copy_1(&message_to_send[i], LIDAR, &i);
     copy_1(&message_to_send[i], ALL, &i);
-    
+
     copy_f(&message_to_send[i], buffer[j].distance, &i);
     copy_f(&message_to_send[i], buffer[j].convolved, &i);
 
@@ -46,12 +51,12 @@ void Scanner::print_log() {
     vTaskDelay(1);
   }
 
-  #else
+#else
 
   ESP_LOGI(TAG, "angle, distance, convolved");
   for (int i = 0; i < buf_index; i++) {
     ESP_LOGI(TAG, "%f, %i, %i", buffer[i].distance, buffer[i].convolved);
   }
- 
-  #endif
+
+#endif
 }
